aeeeb89dcf51b3ba25d8fc5b15d24aa737257521
AMAN GUPTA <128606958+Ag18012003@users.noreply.github.com>
2023-04-12 23:16:57 +0530

    employee website

---
+++ js.js
@@ -0,0 +1,59 @@
+// function rotele(){
+//     document.getElementById("dream").style.transform="rotateY(360deg)";
+    
+// }
+// while(1){
+
+//CREATIVE PRODUCT CARD UI CSS
+//PARALLAX EFFECT
+///WAVY TEXT ANIMATIONSRESPONSIVE CARD GUIs
+
+
+setInterval(function rotele(){
+    document.getElementById("dream").style.transform="rotateY(360deg)";
+    
+},2000);
+// }
+setInterval(function rotele(){
+    document.getElementById("dream").style.transform="none";
+    
+},5000);
+
+
+
+function signuppage(){
+    document.getElementById("sign").click();
+}
+
+// function randomColor(){
+//     var randomColor=new Array();
+//     randomColor[0]="45deg,#0063FF,#8734"
+//     randomColor[0]="45deg,#0067FF,#8734"
+
+
+
+//     var num=Math.floor(Math.random()*randomColor.length);
+//     document.getElementById("sectionimage").style.background="randomColor[num]";
+    
+//     document.getElementById("maindiv").style.background="randomColor[num]";
+    
+//     document.getElementById("maindream").style.background="randomColor[num]";
+// }
+// setInterval(function randomColor(){
+//     var randomColor=new Array();
+//     randomColor[0]="linear-gradient(45deg,#fff,#FFC0CB)"
+//     randomColor[1]="linear-gradient(45deg,#82CAFF,#fff)"
+//     randomColor[2]="linear-gradient(45deg,#FDD017,#fff)"
+//     randomColor[3]="linear-gradient(45deg,#648382,#E3E4EF)"
+
+
+
+//     var num=Math.floor(Math.random()*randomColor.length);
+//     document.getElementById("sectionimage").style.background=randomColor[num];
+//     document.getElementById("sectionimage").style.transition="2s ease";
+
+    
+//     document.getElementById("maindiv").style.background=randomColor[num];
+    
+//     document.getElementById("maindream").style.background=randomColor[num];
+// },2000)
